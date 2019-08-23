11a14b8220a23040d8925031e6878a700ddcad5b
kyle.cooper <kyle.cooper@MU-C1-15.mullen.pvt.k12.co.us>
2019-08-23 11:24:30 -0600

    Final craps

--- Gambling craps/src/GamblingCraps.java
+++ Gambling craps/src/GamblingCraps.java
@@ -6,16 +6,18 @@ public class GamblingCraps
 			{
 				// TODO Auto-generated method stub
 				Scanner userInput = new Scanner(System.in);
+				boolean flag2 = true;
+				while(flag2) {
 				System.out.println("Let's play craps. ");
 				System.out.println("What is your name?");
 				String name = userInput.nextLine();
 				System.out.println("Hello " + name);
 				int randomNumber = (int) ( (Math.random()) * 6) + 1;
 				int randomNumber2 = (int) ( (Math.random()) * 6) + 1;
+				int theTotal2 = 0;
+				int randomNumber3 = 0;
+				int randomNumber4 = 0;
 				int theTotal = randomNumber + randomNumber2;
-				int randomNumber3 = (int) ( (Math.random()) * 6) + 1;
-				int randomNumber4 = (int) ( (Math.random()) * 6) + 1;
-				int theTotal2 = randomNumber3 + randomNumber4;
 				boolean flag1 = true;
 				System.out.println("Roll the dice?");
 				String theResponse = userInput.nextLine();
@@ -34,11 +36,46 @@ public class GamblingCraps
 					
 				}
 				else{
-					System.out.println("" + name + " your point is " + theTotal);
-					while() {
-						
-						
+					System.out.println("" + name + " your point is " + theTotal + "." );
+					
+					
+					
+					while(flag1) {
+						System.out.println("Roll the dice?");
+						String theResponse2 = userInput.nextLine();
+						if(theResponse2.contentEquals("No")) {
+						System.out.println("Okay then you lose");
+						flag1 = false;
+						}
+						else {
+					randomNumber3 = (int) ( (Math.random()) * 6) + 1;
+					randomNumber4 = (int) ( (Math.random()) * 6) + 1;
+					theTotal2 = randomNumber3 + randomNumber4;
+					System.out.println("You rolled a " + randomNumber3 +" and a "+ randomNumber4 + " for a total of "+ theTotal2 + ".");	
+					if(theTotal2 == 7) {
+					System.out.println("Sorry, you lost " + name + ". :(");		
+					flag1=false;
 					}
+					if(theTotal2 == theTotal) {
+					System.out.println("Congrats, You win " + name + "!");
+					flag1=false;
+					}
+					if (flag1) {
+					System.out.println("Keep on going!");
+					}
+						}
+					}
+					System.out.println("Play Again?");
+					String theResponse3 = userInput.nextLine();
+					if(theResponse3.equals("yes")) {
+					flag2 = true;	
+					}
+					else {
+					flag2 = false;
+					System.out.println("Thanks for playing craps.");
+					}
+					}
+				
 				}	
 				}
 			}
