ac3835a903d2346b222b3c476a29c6af384150df
kyle.cooper <kyle.cooper@MU-C1-15.mullen.pvt.k12.co.us>
2019-08-27 11:55:43 -0600

    latest commit

--- Gambling craps/src/GamblingCraps.java
+++ Gambling craps/src/GamblingCraps.java
@@ -5,13 +5,34 @@ public class GamblingCraps
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
+				
 				Scanner userInput = new Scanner(System.in);
 				boolean flag2 = true;
-				while(flag2) {
-				System.out.println("Let's play craps. ");
+				boolean flag3 = true;
 				System.out.println("What is your name?");
 				String name = userInput.nextLine();
 				System.out.println("Hello " + name);
+				int total = 100;
+				while(flag2) {
+				int bet = 0;
+				System.out.println("You have $"+ total + ".");
+				System.out.println("Let's play craps. ");
+				flag3 = true;
+				while(flag3) {
+					
+					System.out.println("Enter your bet:");
+					bet = userInput.nextInt();
+					if(bet > total) {
+					System.out.println("You can't bet more than your total.");
+					}
+					else {
+						flag3 = false;
+					
+					}
+					
+				}
+				
+				
 				int randomNumber = (int) ( (Math.random()) * 6) + 1;
 				int randomNumber2 = (int) ( (Math.random()) * 6) + 1;
 				int theTotal2 = 0;
@@ -19,7 +40,7 @@ public class GamblingCraps
 				int randomNumber4 = 0;
 				int theTotal = randomNumber + randomNumber2;
 				boolean flag1 = true;
-				System.out.println("Roll the dice?");
+				System.out.println("Press Enter to Roll the Dice.");
 				String theResponse = userInput.nextLine();
 				if(theResponse.equals("No")) {
 					System.out.println("Then why are you here");
@@ -29,19 +50,41 @@ public class GamblingCraps
 				System.out.println("You rolled a " + randomNumber +" and a "+ randomNumber2 + " for a total of "+ theTotal + ".");	
 				if(theTotal == 7 || theTotal == 11)	{
 				System.out.println("Congrats, You win " + name + "!");
-					
+				total = total + bet;
+				System.out.println("Your total is now $" + total + ".");
+				System.out.println("Play Again?");
+				String theResponse3 = userInput.nextLine();
+				if(theResponse3.equals("yes") || theResponse3.equals("yES") || theResponse3.equals("YES") || theResponse3.equals("Yes")) {
+				flag2 = true;	
+				}
+				else {
+				flag2 = false;
+				System.out.println("Thanks for playing craps.");
+				}
+				
 				}
 				if(theTotal == 2 || theTotal == 12)	{
 				System.out.println("Sorry, you lost " + name + ". :(");	
-					
+				total = total - bet;
+				System.out.println("Your total is now $" + total + ".");
+				System.out.println("Play Again?");
+				String theResponse3 = userInput.nextLine();
+				if(theResponse3.equals("yes") || theResponse3.equals("yES") || theResponse3.equals("YES") || theResponse3.equals("Yes")) {
+				flag2 = true;	
 				}
-				else{
+				else {
+				flag2 = false;
+				System.out.println("Thanks for playing craps.");
+				}
+				}
+				if(!(theTotal == 7) && !(theTotal == 2) && !(theTotal == 12) && !(theTotal == 11) ){
+					
 					System.out.println("" + name + " your point is " + theTotal + "." );
 					
 					
 					
 					while(flag1) {
-						System.out.println("Roll the dice?");
+						System.out.println("Press Enter to Roll the Dice");
 						String theResponse2 = userInput.nextLine();
 						if(theResponse2.contentEquals("No")) {
 						System.out.println("Okay then you lose");
@@ -55,9 +98,13 @@ public class GamblingCraps
 					if(theTotal2 == 7) {
 					System.out.println("Sorry, you lost " + name + ". :(");		
 					flag1=false;
+					total = total - bet;
+					System.out.println("Your total is now $" + total + ".");
 					}
 					if(theTotal2 == theTotal) {
 					System.out.println("Congrats, You win " + name + "!");
+					total = total + bet;
+					System.out.println("Your total is now $" + total + ".");
 					flag1=false;
 					}
 					if (flag1) {
@@ -67,7 +114,7 @@ public class GamblingCraps
 					}
 					System.out.println("Play Again?");
 					String theResponse3 = userInput.nextLine();
-					if(theResponse3.equals("yes")) {
+					if(theResponse3.equals("yes") || theResponse3.equals("yES") || theResponse3.equals("YES") || theResponse3.equals("Yes")) {
 					flag2 = true;	
 					}
 					else {
